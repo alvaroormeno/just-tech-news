72d7846f56c08ebb3ea236154a662808ff9b1894
alvaroormeno <alvaro@ormeno.org>
2022-03-13 04:16:11 -0400

    done with post models and routes working

--- models/index.js
+++ models/index.js
@@ -1,4 +1,21 @@
 const User = require('./User');
 const Post = require("./Post");
 
+
+
+// create associations between USER TABLE and POST TABLE
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+});
+
+
+
+
+
+
+
+
 module.exports = { User, Post };
